61ca276e43b32ef265394bd671b1c3e1d572487e
Hoang Khoa <nhk9200@gmail.com>
2021-04-16 22:28:59 +0700

    delete some error code

--- lib/bloc/product_bloc.dart
+++ lib/bloc/product_bloc.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:my_app/model/Product.dart';
 
-class ProductBloc {
-  final cartStreamController = StreamController.broadcast();
-  Stream get getStream => cartStreamController.stream;
-
-  final Map allItem = {'shop items': products, 'cart items': []};
-  void addToCart(product,numb){
-    allItem['shop items'].
-  }
-}
